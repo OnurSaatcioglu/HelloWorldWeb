c416987b6874d9e16832c77ba18b1916264efbc2
OnurSaatcioglu <onursaatcioglu@hotmail.com>
2021-06-30 16:37:36 +0300

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWorldWeb.Models;
+
+namespace HelloWorldWeb.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+
+            Student student = new Student();
+
+            student.Id = id;
+            student.Name = "Onur";
+            student.Age = 28;
+
+            return View(student);
+        }
+    }
+}
